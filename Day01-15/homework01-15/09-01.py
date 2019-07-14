c2a46cf890320fc1fb26ac29b2b34f0d106eb963
Jack <cx999cx@126.com>
2019-07-14 22:16:48 +0800

    add explaination to homework09-01

--- Day01-15/homework01-15/09-01.py
+++ Day01-15/homework01-15/09-01.py
@@ -1,16 +1,19 @@
 class Person(object):
     def __init__(self,name,age):
-        self._age = age
         self._name = name
+        self._age = age
 
+    #访问器-getter方法
     @property
     def name(self):
         return self._name
 
+    #访问器-getter方法
     @property
     def age(self):
         return self._age
 
+    #修改器-setter方法
     @age.setter
     def age(self,age):
         self._age = age
